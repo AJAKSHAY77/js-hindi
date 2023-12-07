430cc766120998f89e4441a7b8fd6dc4d55f195d
akshay77 <ajakshayjain11@gmail.com>
2023-12-08 00:44:31 +0530

    done

---
+++ Convertion.js
@@ -0,0 +1,19 @@
+// Number converstion
+
+const name = "akshay"
+
+const newname = Number(name)
+console.log(typeof newname);
+
+// boolean converstion
+
+const poco = 1
+const redmi = 0
+console.log(Boolean(poco));
+console.log(Boolean(redmi));
+
+
+// sting con
+
+const age = 33;
+console.log( typeof String(age));
---
+++ Datatypes.js
@@ -0,0 +1,5 @@
+const aj = null;
+let ja;
+
+console.log(typeof aj);// this is big glitch
+console.log(typeof ja);
