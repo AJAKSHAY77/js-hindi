e34ca0ef430a115959b307eb2c22049672b312b7
akshay77 <ajakshayjain11@gmail.com>
2023-12-07 23:51:56 +0530

    first commit

---
+++ let,const.js
@@ -0,0 +1,11 @@
+const name = "akshay jain";
+let money = 1000;
+var age = 23
+hieght = "three"
+var age = 25
+hieght = 4000
+let id; // undefined
+id=9000
+
+
+console.table([name, money, age, hieght, id]);
